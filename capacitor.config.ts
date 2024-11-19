0a350d78a214490cbca1edbd869221b82ea3a5ae
FeliHub <felipe.moragabarraza@gmail.com>
2024-11-19 20:27:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Automoviles_Inc',
+  webDir: 'www'
+};
+
+export default config;
