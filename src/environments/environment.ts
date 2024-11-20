a01262832d79d1d68a80e2af5e5ded18ad4d4ae7
FeliHub <felipe.moragabarraza@gmail.com>
2024-11-19 22:18:45 -0300

    nuevo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLizfprg0vBwN_pVrCiCscGSNZTSSnNS8",
+    authDomain: "baseautos1.firebaseapp.com",
+    projectId: "baseautos1",
+    storageBucket: "baseautos1.firebasestorage.app",
+    messagingSenderId: "342259105361",
+    appId: "1:342259105361:web:83c681e3fdbb09516c8270"
+  }
 };
 
 /*
