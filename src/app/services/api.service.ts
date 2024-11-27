fb6bb3ec3afbea0a6cc8bad949fc163054760e78
FeliHub <felipe.moragabarraza@gmail.com>
2024-11-27 10:36:42 -0300

    progreso9

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 
-export class ApiService {
+export class ApiService {/*
   private apiUrl = 'https://popular-vehicles-api.p.rapidapi.com/rto/api/v1.0/popular_cars_bikes';
   private headers = new HttpHeaders({
     'x-rapidapi-key': '7088f73db2mshb6e1a58e54a2260p12c8c2jsn25dd5ce208d8',
@@ -18,5 +18,5 @@ export class ApiService {
 
   getVehicles(): Observable<any> {
     return this.http.get(this.apiUrl, { headers: this.headers });
-  }
+  }*/
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLizfprg0vBwN_pVrCiCscGSNZTSSnNS8",
+    authDomain: "baseautos1.firebaseapp.com",
+    projectId: "baseautos1",
+    storageBucket: "baseautos1.firebasestorage.app",
+    messagingSenderId: "342259105361",
+    appId: "1:342259105361:web:83c681e3fdbb09516c8270"
+  }
 };
